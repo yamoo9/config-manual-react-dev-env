e84ea12486c070ab3a0d785df269b6b0c2322452
yamoo9 <yamoog@naver.com>
2021-12-06 15:20:50 +0900

    [구성] Webpack 구성 중...
    - 유틸리티 분리/관리
    - Webapck 개발 서버 명령어 추가

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
-import { logger, getRandomMinMax, transformText } from './utils'
+import { logger, getRandomMinMax, transformText } from './utils';
 
 logger(
-  getRandomMinMax(), 
+  getRandomMinMax(),
   transformText(`
     font-size: 5rem;
     font-weight: 700;
+  `)
+);
+
+logger(
+  getRandomMinMax(50, 99),
+  transformText(`
+    font-size: 5rem;
+    font-weight: 700;
+    color: salmon;
   `)
 );
